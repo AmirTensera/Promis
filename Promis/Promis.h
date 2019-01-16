4b3b40a93a713bdc84f9325a21718c6be04fe5ca
AmirTsafrir <amir@tensera.net>
2019-01-16 16:13:59 +0200

    Add Xcode Project for carthage supoort

---
+++ Promis/Promis.h
@@ -0,0 +1,19 @@
+//
+//  Promis.h
+//  Promis
+//
+//  Created by Amir on 16/01/2019.
+//  Copyright © 2019 Just Eat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promis.
+FOUNDATION_EXPORT double PromisVersionNumber;
+
+//! Project version string for Promis.
+FOUNDATION_EXPORT const unsigned char PromisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promis/PublicHeader.h>
+
+
